9a47fabb39713fe0f1a0b3647812340ce6a5a7eb
Riya Goyal <riya.goyal126@gmail.com>
2026-05-16 11:37:10 +0530

    Reverse by k

---
+++ 8RevByK.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, k;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin >> arr[i];
+    
+    cin >> k;
+    k = k % n; // agar k > n ho toh handle karta hai
+    
+    reverse(arr, arr+n);
+    reverse(arr, arr+k);
+    reverse(arr+k, arr+n);
+    
+    for(int i=0; i<n; i++) cout << arr[i] << " ";
+    
+    return 0;
+}
